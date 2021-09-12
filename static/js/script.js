918dbef282bc49eca4139b0f080a504ccc8fdbba
luke2533 <lukefoxwell1@gmail.com>
2021-09-12 03:21:47 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -4,15 +4,18 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-});
-
-$(document).ready(function(){
     $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
 
-$(document).ready(function(){
-    $('.tooltipped').tooltip();
-});
 
 
 /*
